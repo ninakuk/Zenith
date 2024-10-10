663a54127485b5fe58cb005d2f24941b9f4e1f23
Nina Kukuljan <nina.kukuljan@lge.com>
2024-10-10 16:46:17 +0200

    install needed react navigation libraries

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,6 +5,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import 'expo-dev-client';
+
 //import EntryProvider from '../../providers/EntryProvider';
 
 import { useColorScheme } from '@/src/components/useColorScheme';
